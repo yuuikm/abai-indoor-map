2817ba5319d89d90c33b4e7d86897a16cb09fdb9
Mark <morozov-mark@bk.ru>
2025-04-25 15:37:06 +0500

    feat: added rents to 1 floor

--- src/floors/floor4/Objects.ts
+++ src/floors/floor4/Objects.ts
@@ -1,14 +1,7 @@
 import { FloorObject } from "utils/types";
 
 const floor4Objects: FloorObject[] = [
-  {
-    id: "Adidas 4",
-    d: "M558.987 911.04l116.848.003-1.024 114.297-116.619.503.795-114.803z",
-  },
-  {
-    id: "Zara 4",
-    d: "M172.029 911.04l122.833-.164-.243 116.283-122.709.667.119-116.786z",
-  }
+
 ];
 
 export default floor4Objects;
--- src/floors/floor5/Objects.ts
+++ src/floors/floor5/Objects.ts
@@ -1,14 +1,7 @@
 import { FloorObject } from "utils/types";
 
 const floor5Objects: FloorObject[] = [
-  {
-    id: "Adidas 5",
-    d: "M558.987 911.04l116.848.003-1.024 114.297-116.619.503.795-114.803z",
-  },
-  {
-    id: "Zara 5",
-    d: "M172.029 911.04l122.833-.164-.243 116.283-122.709.667.119-116.786z",
-  }
+
 ];
 
 export default floor5Objects;
