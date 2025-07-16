3ba4e9de10c7bf8971f5c6a75f06469474351972
Mark <morozov-mark@bk.ru>
2025-07-16 21:19:23 +0500

    fix: fixes with rents number

--- src/components/IndoorMap/MapBackground.tsx
+++ src/components/IndoorMap/MapBackground.tsx
@@ -10,8 +10,8 @@ function MapBackground({ children, floor }: MapBackgroundProps) {
 
   return (
     <svg
-      viewBox="0.469 0.006 1461.95 1149.136"
-      // viewBox="0.469 750 1461.95 1149.136"
+      // viewBox="0.469 0.006 1461.95 1149.136"
+      viewBox="0.469 750 1461.95 1149.136"
       className="lg:h-[85vh] lg:w-[75vw] h-[85dvh]"
     >
       <image id="background" width="100%" height="100%" href={mapPath} />
